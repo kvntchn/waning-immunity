8a3d3e8acb0671f7298e2bfbe558ac8ef5b01143
Kevin Chen <kevchen@berkeley.edu>
2023-02-22 02:36:56 -0800

    able to run MCMC

--- script/mcmc.R
+++ script/mcmc.R
@@ -13,60 +13,78 @@ san_francisco.dat[,time := 1:.N]
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Log prior distributions on parameters
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-log_prior_theta <- function(theta = parameters) {
-	R0_high <- dunif(
-		theta[["R0_high"]], 1, 3, log = T)
-	R0_low  <- dunif(
-		theta[["R0_low"]], 0.375, 1.5, log = T)
-	R0_omicron  <- dunif(
-		theta[["R0_omicron"]], 1.5, 6, log = T)
-	disease_duration <- dunif(
-		theta[["disease_duration"]], 14, 28, log = T)
-	disease_duration_omicron <- dunif(
-		theta[["disease_duration_omicron"]], 7, 21, log = T)
-	death_rate <- dunif(
-		theta[['death_rate']], 1e-4, 1e-2, log = T)
-	vaccination_rate <- dunif(
-		theta[['vaccination_rate']], 1e-4, 1e-2, log = T)
-	recovery_period <- dunif(
-		theta[['recovery_period']], 30 * 6, 30 * 12 * 1.5, log = T)
-	recovery_period_omicron <- dunif(
-		theta[['recovery_period_omicron']], 30 * 6, 30 * 12, log = T)
-	emergence_probability <- dunif(
-		theta[['emergence_probability']], 1e-5, 1e-3, log = T)
-	omicron_date <- dunif(theta[['omicron_date']], 360, 420, log = T)
-	return(R0_high + R0_low + R0_omicron + disease_duration +
-				 	disease_duration_omicron + death_rate +
-				 	vaccination_rate + recovery_period +
-				 	recovery_period_omicron + emergence_probability +
-				 	omicron_date
+# prior_hyperparameters <- data.frame(
+# 	param = c("R0_high", "R0_low", "R0_omicron",
+# 						"disease_duration", "disease_duration_omicron",
+# 						"death_rate", "vaccination_rate",
+# 						"recovery_period", "recovery_period_omicron",
+# 						"emergence_probability",
+# 						"omicron_date"),
+# 	alpha = c(1.5, 7.5, 30,
+# 						170, 150,
+# 						20, 20,
+# 						30 * 12 / 10, 30 * 10 / 10,
+# 						5e-4 * 1e-2,
+# 						390 * 3
+# 	),
+# 	beta = c(10, 10, 10,
+# 					 10, 10,
+# 					 20000, 20000,
+# 					 1 / 10, 1 / 10,
+# 					 1 * 1e-2, 3)
+# )
+
+prior_hyperparameters <- data.frame(
+	param = c("R0_high", "R0_low", "R0_omicron",
+						"disease_duration", "disease_duration_omicron",
+						"death_rate", "vaccination_rate",
+						"recovery_period", "recovery_period_omicron",
+						"emergence_probability",
+						"omicron_date"),
+	alpha = c(1, 1/100, 1,
+						3, 3,
+						1e-6, 1e-6,
+						1, 1,
+						1e-6,
+						390
+	),
+	beta = c(100, 1, 100,
+					 300, 300,
+					 1e-1, 1e-1,
+					 30 * 12 * 10, 30 * 10 * 10,
+					 5e-2,
+					 410)
+)
+
+log_prior_theta <- function(theta = parameters, hyperparam = prior_hyperparameters) {
+	prior_prob <- sapply(names(theta), function(x) {
+		dunif(theta[[x]],
+					with(hyperparam, alpha[param == x]),
+					with(hyperparam, beta[param == x]),
+					log = T)
+	})
+	return(with(prior_prob,
+							R0_high + R0_low + R0_omicron +
+								disease_duration + disease_duration_omicron +
+								death_rate + vaccination_rate +
+								recovery_period + recovery_period_omicron +
+								emergence_probability + omicron_date
+	)
 	)}
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Log likelihood for single observation
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-log_lik_I <- function(data, model) {
-	# Incidence is observed through a Poisson process.
-	return(dpois(
-		x = data,
-		lambda = model,
-		log = TRUE
-	))
-}
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Log likelihood for entire trajectory
+# Log likelihood for trajectory
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 log_lik_traj <- function(times, data, theta_proposed, initial_state) {
-	traj <- data.frame(ode(
+	traj <- data.frame(quietly(ode)(
 		y = initial_state,
 		times = times,
 		parms = theta_proposed,
 		func = compartmental_model,
 		method = "lsode"
-	))
+	)$result)
 	model <- traj$I_wt + traj$I_r + traj$I_rV
-	return(sum(log_lik_I(data, model)))
+	return(sum(dpois(data, model[1:length(data)], log = T)))
 }
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -78,9 +96,15 @@ log_post <- function(times, data, parameters, initial_state) {
 		return(log_prior)
 	} else {
 		log_lik <- log_lik_traj(times, data, parameters, initial_state)
-		if (abs(log_lik) < .Machine$double.eps) {
-			return(-Inf)} else {
-				return(log_prior + log_lik)}
+		if (!is.finite(log_lik)) {
+			return(-Inf)
+		} else {
+			if (abs(log_lik) < .Machine$double.eps) {
+				return(-Inf)
+			} else {
+				return(log_prior + log_lik)
+			}
+		}
 	}
 }
 
@@ -105,23 +129,26 @@ log_post_wrapper <- function(theta_proposed) {
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Metropolis-Hastings MCMC
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-mh_mcmc <- function(posterior, init, proposal_sd, num_iter) {
+mh_mcmc <- function(posterior, init, proposal_sd, num_iter, quiet = T, progress = F) {
 	# Evaluate the function `posterior` at `init`
 	post_current <- posterior(init)
 	# Initialize variables to store the current value of theta, the
 	# vector of sample values, and the number of accepted proposals
 	theta_current <- init
 	samples <- matrix(NA, ncol = length(init), nrow = num_iter + 1)
+	colnames(samples) <- names(init)
 	samples[1,] <- init
 	accepted <- 0
 	# Run the MCMC algorithm for `num_iter` iterations.
-	for (i in 1:num_iter) {
+	if (progress) {pb <- txtProgressBar(min = 0, max = num_iter, style = 3)}
+	for (i in 2:(num_iter + 1)) {
+		current_accepted <- 0
 		# Draw a new theta from a Gaussian proposal distribution and
 		# assign this to a variable called theta_proposed.
 		theta_proposed <- rnorm(n = length(theta_current),
 														mean = theta_current,
 														sd = proposal_sd)
-		names(theta_proposed) <- names(theta_current)
+		names(theta_proposed) <- names(init)
 		# Evaluate the (log) posterior function
 		post_proposed <- posterior(theta_proposed)
 		# Compute the Metropolis-Hastings (log) acceptance rate
@@ -131,18 +158,25 @@ mh_mcmc <- function(posterior, init, proposal_sd, num_iter) {
 			u <- runif(1)
 			# Use the random number and the acceptance probability to
 			# determine if `theta_proposed` will be accepted.
-			if (u < exp(log_accept)) {
+			if (log(u) < log_accept) {
 				# If accepted, update
 				theta_current <- theta_proposed
 				post_current <- post_proposed
 				# And update number of accepted proposals.
 				accepted <- accepted + 1
+				current_accepted <- 1
 			}}
 		# Add the current theta to the vector of samples.
 		samples[i,] <- theta_current
-		# Print the current state of chain and acceptance rate.
-		cat("Iteration:", i, "Chain:", theta_current,
-				"acceptance rate:", accepted / i, "\n")
+		if (!quiet) {
+			if (i %% 500 == 0 | current_accepted == 1) {
+				# Print the current state of chain and acceptance rate.
+				cat("Iteration:", sprintf('% 5d', i),
+						"\tChain:", theta_current[-which(proposal_sd == 0)],
+						"\nAccepted:", c("no", "yes")[current_accepted + 1],
+						"\tAcceptance rate:", round(accepted / i * 100, 2), "%\n")
+			}}
+		if (progress) {setTxtProgressBar(pb, i)}
 	}
 	return(samples)
 }
@@ -157,6 +191,8 @@ parameters <- c(
 	R0_previous = -Inf,
 	immune_period = Inf,
 	dt = 1,
+	p_non_vax = 0.2,
+	saturation = 0.01,
 	# Random parameters
 	R0_high = 1.5,
 	R0_low = 0.75,
@@ -167,46 +203,24 @@ parameters <- c(
 	vaccination_rate = 1 / 1000,
 	recovery_period = 30 * 12,
 	recovery_period_omicron = 30 * 10,
-	p_non_vax = 0.2,
-	saturation = 0.01,
 	emergence_probability = 5e-4,
 	omicron_date = 390)
 
+init <- parameters
+
+set.seed(230)
 mcmc_trace <- mh_mcmc(
 	posterior = log_post_wrapper,
-	init = parameters,
+	init = init,
 	proposal_sd = c(
-		rep(0, 4),
-		rep(0.001, length(parameters) - 4)
+		rep(0, 6),
+		sqrt((prior_hyperparameters$beta - prior_hyperparameters$alpha)^2 / 12) / 100
 	),
-	num_iter = 20)
+	num_iter = 5e4,
+	quiet = F,
+	progress = F)
+save(mcmc_trace, file = 'output/mcmc_trace.rdata')
 
+trace <- mcmc(mcmc_trace[,-(1:6)])
 
-compartmental_model <- function() {
-
-	traj <- data.frame(ode(
-		y = initial_state,
-		times = times,
-		parms = theta_proposed,
-		func = compartmental_model,
-		method = "lsode"
-	))
-	model <- traj$I_wt + traj$I_r + traj$I_rV
-
-  for (k in 1:K) {
-  	I[k] ~ dpois(lambda[k])
-  	lambda[k]
-  }
-
-    logit(pi[i]) <- b[1] + b[2]*tobgp2[i] + b[3]*tobgp3[i] + b[4]*tobgp4[i] + b[5]*age[i];
-    case[i] ~ dbin(pi[i],1);
-  }
-
-  # PRIORS ON BETAS
-  for (j in 1:Nx) {b[j] ~ dnorm(mu[j],tau[j]);} # independent normal prior
-
-  # Calculate ORs:
-  for (l in 1:Nx) {
-      OR[l] <- exp(b[l]);
-  }
-}
+plot(mcmc(mcmc_trace[,7:9]))
